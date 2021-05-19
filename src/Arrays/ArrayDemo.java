ce180d465ff11c39eb5bf7db34454f696f79ec36
ejazshaikh1 <ejazshaikh1993@gmail.com>
2021-05-19 12:39:30 +0530

    third commit

---
+++ src/Arrays/ArrayDemo.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.time.Clock;
+
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+
+        int[] numbersArray;
+        numbersArray=new int[5];
+        numbersArray[0]=(100);
+        numbersArray[1]=(200);
+        numbersArray[2]=(225);
+        numbersArray[3]=(380);
+        numbersArray[4]=(420);
+        System.out.println(numbersArray[0]);
+        System.out.println(numbersArray[1]);
+        System.out.println(numbersArray[2]);
+        System.out.println(numbersArray[3]);
+        System.out.println(numbersArray[4]);
+
+    }
+}
